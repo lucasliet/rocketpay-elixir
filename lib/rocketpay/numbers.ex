eac352766dadff7d7998b561dc13178392f9255f
lucasliet <lucasouliveira@gmail.com>
2021-02-23 09:46:24 -0300

    Enum.map and annonymous function

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -4,6 +4,11 @@ defmodule Rocketpay.Numbers do
     |>handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+  defp handle_file({:ok, file}) do
+    convert_to_list(file)
+    |>convert_list_to_numbers()
+  end
+  defp convert_to_list(file), do: String.split(file, ",")
+  defp convert_list_to_numbers(list), do: Enum.map(list, fn number -> String.to_integer(number) end)
 end
