208f814f99023b7e1901965385aec55a8655127c
lucasliet <lucasouliveira@gmail.com>
2021-02-23 09:53:48 -0300

    result as :ok and map operator

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -6,8 +6,11 @@ defmodule Rocketpay.Numbers do
 
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
   defp handle_file({:ok, file}) do
-    convert_to_list(file)
-    |>convert_list_to_numbers()
+    sum = file
+      |>convert_to_list()
+      |>convert_list_to_numbers()
+      |>Enum.sum()
+    {:ok, %{result: sum}}
   end
   defp convert_to_list(file), do: String.split(file, ",")
   defp convert_list_to_numbers(list), do: Enum.map(list, fn number -> String.to_integer(number) end)
