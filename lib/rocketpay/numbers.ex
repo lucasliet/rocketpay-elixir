9eca0b5a37f9319c938a397694d28b4eda33fa54
lucasliet <lucasouliveira@gmail.com>
2021-02-23 10:10:03 -0300

    Enum to Stream

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -7,11 +7,11 @@ defmodule Rocketpay.Numbers do
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
   defp handle_file({:ok, file}) do
     sum = file
-      |>convert_to_list()
-      |>convert_list_to_numbers()
-      |>Enum.sum()
+      |>convert_to_list
+      |>convert_list_to_numbers
+      |>Enum.sum
     {:ok, %{result: sum}}
   end
   defp convert_to_list(file), do: String.split(file, ",")
-  defp convert_list_to_numbers(list), do: Enum.map(list, fn number -> String.to_integer(number) end)
+  defp convert_list_to_numbers(list), do: Stream.map(list, fn number -> String.to_integer(number) end)
 end
